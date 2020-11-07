7176e332512d7e97b4644c8b14e11c8fbf01b0d5
Axmadjon1218 <axiprogrammer@gmail.com>
2020-11-06 18:56:59 -0800

    borderRadius15itim12

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -9,7 +9,7 @@ class SplashPage extends StatefulWidget{
 class _SplashPageState extends State<SplashPage>{
   _Timer(){
     Timer(Duration(seconds:3),(){
-      Navigator.pushNamed(context,HomePage.id);
+      Navigator.pushReplacementNamed(context,HomePage.id);
     });
   }
   @override

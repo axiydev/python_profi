2e24034ccfc57d959dd4848f78fa0f3ce7adb065
Axmadjon1218 <axiprogrammer@gmail.com>
2020-11-10 07:45:53 -0800

    elevation,bottomOpacityhd1red1

--- lib/pages/basic_themes.dart
+++ lib/pages/basic_themes.dart
@@ -39,7 +39,7 @@ class _ThemeOneState extends State<ThemeOne>{
                     child:Container(
                       decoration:BoxDecoration(
                         borderRadius:BorderRadius.circular(15),
-                        color:Colors.white,
+                        color:Colors.yellowAccent,
                       ),
                       child:Center(
                         child:Column(
@@ -61,8 +61,9 @@ class _ThemeOneState extends State<ThemeOne>{
                   Expanded(
                     flex:2,
                     child:Container(
+                      padding:EdgeInsets.all(10),
                       decoration:BoxDecoration(
-                        color:Colors.white,
+                        color:Colors.cyanAccent,
                         borderRadius:BorderRadius.circular(15),
                       ),
                       child:Center(
@@ -74,30 +75,17 @@ class _ThemeOneState extends State<ThemeOne>{
               ),
             ),
             Container(
-                height: 250,
-                color:Colors.red,
-              margin:EdgeInsets.only(left: 10,right:10),
-            ),
-            Container(
-                height: 250,
-                color:Colors.green,
-              margin:EdgeInsets.only(left: 10,right:10)
-            ),
-              Container(
-                height: 250,
-                color:Colors.yellow,
-                margin:EdgeInsets.only(left: 10,right:10),
+              height: 1000,
+              width:double.infinity,
+              margin:EdgeInsets.only(left: 10,right:10,bottom:10),
+              padding:EdgeInsets.all(10),
+              decoration:BoxDecoration(
+                borderRadius:BorderRadius.circular(15),
+                color:Colors.white,
               ),
-              Container(
-                height: 250,
-                color:Colors.red,
-                margin:EdgeInsets.only(left: 10,right:10),
-              ),
-              Container(
-                height: 250,
-                color:Colors.green,
-                margin:EdgeInsets.only(left: 10,right:10),
+              child:Text("""Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer. Text messages may be sent over a cellular network, or may also be sent via an Internet connection.The term originally referred to messages sent using the Short Message Service (SMS). It has grown beyond alphanumeric text to include multimedia messages using the Multimedia Messaging Service (MMS) containing digital images, videos, and sound content, as well as ideograms known as emoji (happy faces, sad faces, and other icons), and instant messenger applications (usually the term is used when on mobile devices).Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. In the 2010s, the sending of short informal messages became an accepted part of many cultures, as happened earlier with emailing.[1] This makes texting a quick and easy way to communicate with friends, family and colleagues, including in contexts where a call would be impolite or inappropriate (e.g., calling very late at night or when one knows the other person is busy with family or work activities). Like e-mail and voicemail and unlike calls (in which the caller hopes to speak directly with the recipient), texting does not require the caller and recipient to both be free at the same moment; this permits communication even between busy individuals. Text messages can also be used to interact with automated systems, for example, to order products or services from e-commerce websites, or to participate in online contests. Advertisers and service providers use direct text marketing to send messages to mobile users about promotions, payment due dates, and other notifications instead of using postal mail, email, or voicemail.""",style:TextStyle(fontSize:20,fontFamily:'Itim'),),
             ),
+
           ],
         ),
       ),
